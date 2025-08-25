15c0d7dca733840743e8614802fe844ab9ab0224
amirhossein <kazemiamirhos688@gmail.com>
2025-08-25 05:55:32 -0700

    Calculator

---
+++ script.js
@@ -0,0 +1,23 @@
+let display = document.getElementById("display");
+
+
+function Add (value) {
+    display.value += value;
+}
+
+function Clear () {
+    display.value = "";
+}
+
+function Backspace () {
+    display.value = display.value.slice(0, -1);
+}
+
+function Calculator () {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "error"
+    }
+}
+
